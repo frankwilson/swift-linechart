7981b2b80f52e0bd4c1998f1ddc3b5a2908cc0c7
Daniel Friedrich <daniel.friedrich@sersoft.de>
2015-09-07 18:00:08 +0200

    add linechartFramework target

---
+++ linechartFramework/linechartFramework.h
@@ -0,0 +1,19 @@
+//
+//  linechartFramework.h
+//  linechartFramework
+//
+//  Created by Daniel Friedrich on 07.09.15.
+//  Copyright © 2015 zemirco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for linechartFramework.
+FOUNDATION_EXPORT double linechartFrameworkVersionNumber;
+
+//! Project version string for linechartFramework.
+FOUNDATION_EXPORT const unsigned char linechartFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <linechartFramework/PublicHeader.h>
+
+
