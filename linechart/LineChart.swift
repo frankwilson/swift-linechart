54436735a9db72df454152c4163e7bf968b667c6
Pavel Kazantsev <pv.kazantsev@gmail.com>
2016-05-22 13:43:26 -0500

    Removed unused UIColorFromHex method.

--- linechart/LineChart.swift
+++ linechart/LineChart.swift
@@ -18,17 +18,7 @@ public class LineChart: UIView {
     * Helpers class
     */
     private class Helpers {
-        
-        /**
-        * Convert hex color to UIColor
-        */
-        private class func UIColorFromHex(hex: Int) -> UIColor {
-            let red = CGFloat((hex & 0xFF0000) >> 16) / 255.0
-            let green = CGFloat((hex & 0xFF00) >> 8) / 255.0
-            let blue = CGFloat((hex & 0xFF)) / 255.0
-            return UIColor(red: red, green: green, blue: blue, alpha: 1)
-        }
-        
+
         /**
         * Lighten color.
         */
